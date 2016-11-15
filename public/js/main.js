52e6c396027b09f3efdf03d327190bf83bb711cc
Ben Fleming <benfleming121@gmail.com>
2016-11-16 10:32:10 +1300

    Added Files
    Added The Current Project Files

---
+++ public/js/main.js
@@ -0,0 +1,126 @@
+// Brain Fuck Interperter
+// © Ben Fleming 2016
+
+// How It Works
+// >, i++;
+// <, i--;
+// +, val++;
+// -, val--;
+// [, loop start;
+// ], loop end;
+// ,, get user input;
+// ., print val as ASCII;
+
+// TODO: Connect To A UI
+// Connect An Output Section
+// Connect An Input Section
+// Connect A Run Button
+
+// Main Function Block
+(function( $ ) {
+
+  // Variables
+  // pointer as int; Index Of The Current Cell
+  // cells as arr; Array Of All The Cells
+  // loop_stack as arr; Array Of All Current Loops
+  // valid_chars as str; String Of All The Vaild Brainfuck Characters
+  var pointer=0;
+  var cells=[0];
+  var loop_stack=[];
+  var valid_chars="<>+-[],.";
+
+  // Run Function
+  // To Run The Code On The Users Request
+  function runCode() {
+    console.log("Running Code");
+    // Get Brainfuck Code
+    // And Get Only The Valid Characters
+    var code_raw = $("#code_input").html();
+    var code_chars =  code_raw.replace(new RegExp('[^' + valid_chars + ']', 'g'), '');
+    // Run Through BrainFuck Code
+    while (char_index<code_chars.length) {
+
+      // Get Char Value
+      // At The Char Index
+      var char_val = characters.charAt(char_index);
+
+      // Switch Case For Each Characters Action
+      switch (char_val) {
+
+        // Case For ">" Character
+        // Increase Pointer Value By 1
+        case ">":
+          pointer++;
+          if (cells[pointer]==null) {cells[pointer]=0;}
+          break;
+
+        // Case For "<" Character
+        // Decrease Pointer Value By 1
+        case "<":
+          pointer--;
+          break;
+
+        // Case For "+" Character
+        // Increase Cell Value By 1
+        case "+":
+          cells[pointer]++;
+          break;
+
+        // Case For "-" Character
+        // Decrease Cell Value By 1
+        case "-":
+          cells[pointer]--;
+          break;
+
+        // Case For "[" Character
+        // Push The Character Index To The Loop Memory
+        case "[":
+          loop_stack.push(char_index);
+          break;
+
+        // Case For "]" Character
+        // Rturn To The Last "[" Character In The Code
+        case "]":
+          if (cells[pointer]!=0) {char_index=loop_stack.pop();}
+          else {loop_stack.pop();}
+          break;
+
+        // Case For "," Character
+        // Get The Users Input
+        case ",":
+          cells[pointer]=input();
+          break;
+
+        // Case For "." Character
+        // Output The Current Cell Value As ASCII
+        case ".":
+          // Call The Output Function
+          output(String.fromCharCode(cells[pointer]));
+          break;
+
+        // Default Case To Handle Errors
+        // Add To The Error Count
+        default:
+          error_count++;
+
+      }
+
+      if (char_val!="]") {char_index++;}
+    }
+  }
+
+  // Output Function
+  // To Output To The Screen
+  function output(value) {
+    console.log("Output Value : "+value);
+  }
+
+  // Input Function
+  // To Get The Users Input
+  function input() {
+    return prompt("Enter User Input, Max 1 Character: ").charCodeAt(0);
+  }
+
+  $("#run_btn").on("click", runCode());
+
+})( jQuery );
