26abe0cd23adc3df3acff1c53fa52b8f21f7acf6
Ben Fleming <benfleming121@gmail.com>
2016-11-20 13:22:28 +1300

    Delay Range Slider Added
    So That The User Can Slow Down Or Speed Up The Code

--- public/js/main.js
+++ public/js/main.js
@@ -138,7 +138,8 @@
          $(".cell").eq(pointer).html(cells[pointer]);
 
           if (--i && char_index<code_chars.length) myLoop(i);      //  decrement i and call myLoop again if i > 0
-       }, 50)
+       }, $("#delay_slider").val())
+       console.log($("#delay_slider").val());
     })(10000);
   }
 
